6341cafe32ada24e27952c83a74a738debfc8dae
Justin Green <JGreen72181@hotmail.com>
2021-03-16 09:00:27 -0700

    added put path

--- landingtemplate.js
+++ landingtemplate.js
@@ -1,6 +1,6 @@
 
 
-var html = "<h1>Welcome to Justins FakeUSER API</h1><h3>Click to get data <a href='/users'>Users</a></h3><h3>Click to add data <a href='/adduser'>Add</a></h3><p>Version 1.0.0</p>"
+var html = "<h1>Welcome to Justins FakeUSER API</h1><h3>Click to get data <a href='/users'>Users</a></h3><h3>Know the username? <code>/users/:username</code><h3>Click to add data <a href='/adduser'>Add</a></h3><p>Version 1.0.0</p>"
 
 
 
